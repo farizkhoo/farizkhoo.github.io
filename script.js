af327ce9e6d430973b73e30b20e3fb3cfef7cf2b
farizkhoo <fariz_khoo@hotmail.com>
2018-05-10 16:32:57 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,98 @@
 
 // initialise variables
-let turnCounter = 1;
-let turnState = 1; // 1 is "O", 2 is "X"
-let winState = 0;
-let currentBoardState = ['', '', '', '', '', '', '', '', ''];
-let currentBoardValueO = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-let currentBoardValueX = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-const boardValue = [2, 11, 5, 9, 6, 3, 7, 1, 10];
-// const letter = ['X' 'O'];
+let gameMode = 0; // default 2-player mode
+let computerFirst = 0; // default is computer goes second
+let turnCounter = 1; // stores current turn value
+let turnState = 1; // 1 is O's turn, 2 is X's turn
+let winState = 0; // stores the state of win
+let currentBoardState = ['', '', '', '', '', '', '', '', '']; // stores current board state
+let currentBoardValueO = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // stores current board value for O
+let currentBoardValueX = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // stores current board value for X
+const boardValue = [2, 11, 5, 9, 6, 3, 7, 1, 10]; // defines a value for each square
+
+// define certain targets for elements
 const boardText = document.getElementsByClassName('board-text');
 const boardArea = document.getElementById('board-area');
 const inputArea = document.getElementsByClassName('input-area');
 const winMessage = document.getElementById('win-message');
 
+// WIP
 function setComputerEasy(){
-
+	gameMode = 1;
+	clearBoardArea();
+	if (confirm('Would you like the computer to move first?')) {
+		computerFirst = 1;
+		computerMove();
+	} else {}
 }
 
+// WIP
+function setComputerHard(){
+	gameMode = 2;
+	clearBoardArea()
+}
+
+// WIP
+function computerMove(){
+	let moveValid = false;
+	getTurnState();
+
+
+	if (computerFirst == 0 && gameMode == 1) { // default setting, computer only inputs "X"
+
+		while(moveValid == false){
+			randLoc = Math.floor(Math.random() * currentBoardState.length);
+			if (currentBoardState[randLoc] == ''){
+				currentBoardState[randLoc] = 'X';
+				console.log(currentBoardState);
+				displayBoardState();
+				moveValid = true;
+			}
+		}
+	} else if (computerFirst == 1 && gameMode == 1) { // computer only inputs "O"
+
+		while(moveValid == false){
+			randLoc = Math.floor(Math.random() * currentBoardState.length);
+			if (currentBoardState[randLoc] == ''){
+				currentBoardState[randLoc] = 'O';
+				console.log(currentBoardState);
+				displayBoardState();
+				moveValid = true;
+			}
+		}
+	}
+
+	turnCounter++;
+	winCheck();
+}
+
+// assigns board value based on the current board state
 function findBoardValue(){
-	if (turnState == 1){
+	if (turnState == 1){ // for player one
 		for (i = 0; i < currentBoardState.length; i++){
 			if (currentBoardState[i] == "O"){
 				currentBoardValueO[i] = boardValue[i];
 			}
 		}
 		console.log(currentBoardValueO);
-		return currentBoardValueO;
-	} else {
+		return currentBoardValueO; // return board value for O
+
+	} else { // for player two
 		for (i = 0; i < currentBoardState.length; i++){
 			if (currentBoardState[i] == "X"){
 				currentBoardValueX[i] = boardValue[i];
 			}
 		}
 		console.log(currentBoardValueX);
-		return currentBoardValueX;
+		return currentBoardValueX; // return board value for X
  	}
 }
 
+// accepts a board value input finds the sum of each winning direction
 function findWinBoardValue(boardValue){
 
-	let winBoardValue = [0, 0, 0, 0, 0, 0, 0, 0];
+	let winBoardValue = [0, 0, 0, 0, 0, 0, 0, 0]; // define 8 summations
+
 	// horizontal check
 	for (i = 0; i < 3; i++){
 		winBoardValue[i] = boardValue[i + (2*i)] + boardValue[i+1+(2*i)] + boardValue[i+2+(2*i)];
@@ -54,7 +107,8 @@ function findWinBoardValue(boardValue){
 	// diagonal check
 	winBoardValue[6] = boardValue[0] + boardValue[4] + boardValue[8];
 	winBoardValue[7] = boardValue[2] + boardValue[4] + boardValue[6];
-	return winBoardValue;
+
+	return winBoardValue; // returns all 8 directions of summation
 }
 
 // inputs content of HTML into currentBoardState array
@@ -65,17 +119,18 @@ function getBoardState() {
 }
 
 // sets HTML from currentBoardState array
-function displayBoardState(){
+function displayBoardState() {
 	for (i = 0; i < 9; i++){
 		boardText[i].innerHTML = currentBoardState[i];
 	}
 }
 
-// there are only two players in tic tac toe, playerA and playerB. therefore, there are only two turnStates;
+// update turn state (first player or seond player)
 function getTurnState(){
 	turnState = turnCounter % 2;
 }
 
+// clears board and resets parameters
 function clearBoardArea(){
 	currentBoardState = ['', '', '', '', '', '', '', '', ''];
 	turnCounter = 1;
@@ -101,9 +156,13 @@ function displayWinMessage(){
 	}
 }
 
+// outputs a win message based on who won or draw if nobody won
 function winCheck(){
-	// check win condition
+
+	// obtain win condition
 	winCondition = findWinBoardValue(findBoardValue());
+
+	// checks win board values which are equal to 18
 	if (turnCounter < 10){
 		for (i = 0; i < winCondition.length; i++) {
 			if ((turnState == 1) && (winCondition[i] == 18)){
@@ -114,7 +173,7 @@ function winCheck(){
 			} else {}
 		}
 	} else {
-		winState = 3;
+		winState = 3; // draw as turn counter reaches 9
 	}
 	// display win message
 	displayWinMessage();
@@ -125,28 +184,37 @@ boardArea.onclick = function(event){
 	// prevent default behaviour
 	event.preventDefault();
 
-	// console.log("event: " + event);
-	// console.log(event);
-	// console.log(event.target);
-
-	// change currentBoardState
+	// updates turn state
 	getTurnState();
 
+	// on click display X or O based on turn state and whether empty space was clicked
 	if (turnState == 1 && event.target.innerHTML == ''){
 		event.target.innerHTML = "O";
+		getBoardState();
 		turnCounter++; // update turn counter
+		winCheck();
+		if (gameMode > 0 && winState == 0) {
+			computerMove();
+		}
+
 	} else 	if (turnState == 0 && event.target.innerHTML == ''){
 		event.target.innerHTML = "X";
+		getBoardState();
 		turnCounter++; // update turn counter
+		winCheck();
+		if (gameMode > 0 && winState == 0) {
+			computerMove();
+		}
+
 	} else {
 		window.alert('Please click on an empty square');
 	}
 
-	// display currentBoardState
-	getBoardState();
-	displayBoardState();
 
-	winCheck();
+	// display currentBoardState
+	// getBoardState();
+	// displayBoardState();
+
 }
 
 
