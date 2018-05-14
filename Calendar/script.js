dc32ba6fc8f728bae2dd7c772619bae412359a34
farizkhoo <fariz_khoo@hotmail.com>
2018-05-14 17:49:37 +0800

    Add files via upload

---
+++ Calendar/script.js
@@ -0,0 +1,47 @@
+function nextMonth5(){
+	window.location.href = "June2018.html";
+}
+
+function nextMonth6(){
+	window.location.href = "July2018.html";
+}
+
+function nextMonth7(){
+	window.location.href = "August2018.html";
+}
+
+function nextMonth8(){
+	window.location.href = "September2018.html";
+}
+
+function nextMonth9(){
+	window.alert("You may only book 4 months in advance.");
+}
+
+function previousMonth5(){
+	window.alert("Please build a time machine for that.");
+}
+
+function previousMonth6(){
+	window.location.href = "May2018.html";
+}
+
+function previousMonth7(){
+	window.location.href = "June2018.html";
+}
+
+function previousMonth8(){
+	window.location.href = "July2018.html";
+}
+
+function previousMonth9(){
+	window.location.href = "August2018.html";
+}
+
+function returnButton(){
+	window.location.href = "../index.html";
+}
+
+function dayClicked(){
+	window.location.href = "../checkout.html";
+}
